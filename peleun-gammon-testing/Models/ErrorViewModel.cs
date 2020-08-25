5ba822ad68e300a14c6f0f71cfd54745911399c5
Peter Leung <peleun@microsoft.com>
2020-08-25 10:04:34 +0800

    Add project files.

---
+++ peleun-gammon-testing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace peleun_gammon_testing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
